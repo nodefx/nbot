187fa427dc8f1ff43fa4c07c8306c64e8e25e0c3
ken <kenxu@mazing.com>
2017-04-27 16:28:11 +0800

    v0.1.1

--- app/platform/coding.js
+++ app/platform/coding.js
@@ -19,4 +19,4 @@ async function gitpush(gitEvent) {
   if (fs.existsSync(`${gitEvent.path}/deploy/push.js`)) return
   await db.model('push').insertAsync(gitEvent)
   cmd(`node`, [`${gitEvent.path}/deploy/push.js`, `'${JSON.stringify(gitEvent)}'`])
-}
+}  
