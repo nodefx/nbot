129220d5771921cef82efd702d843027776b7c5e
ken <kenxu@mazing.com>
2017-04-27 19:12:49 +0800

    v0.3.2

--- deploy/push.js
+++ deploy/push.js
@@ -3,7 +3,8 @@
  */
 const {exec} = require('child_process')
 let gitEvent = process.argv[2]
-gitEvent = JSON.parse(gitEvent||{})
+console.log('gitEvent',gitEvent)
+gitEvent = JSON.parse(gitEvent)
 const cmd = function (command) {
   return new Promise((resolve, reject) => {
     exec(command, function (e, o, oe) {
