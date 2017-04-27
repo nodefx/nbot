f2a3373a6d86939828e1460c4ca510ca643af7d4
ken <kenxu@mazing.com>
2017-04-27 18:38:35 +0800

    v0.1.2

--- deploy/push.js
+++ deploy/push.js
@@ -4,29 +4,23 @@
 const {exec} = require('child_process')
 const gitEvent = process.argv[2]
 const cmd = function (command) {
-  console.log(__dirname, command)
   return new Promise((resolve, reject) => {
     exec(command, function (e, o, oe) {
       if (e) {
-        console.error(e)
         return reject(e)
       }
       else if (oe) {
-        console.error(oe)
         return reject(oe)
       }
       else if (o) {
-        console.log(o)
         return resolve(o)
       }
     })
-  }).catch(e => console.error(__dirname, command, e))
+  }).then(o => console.log(__dirname, command, o)).catch(e => console.error(__dirname, command, e))
 }
 
 async function queue() {
-
   await cmd('git pull')
   await cmd('yarn reload')
-
 }
 queue().then()
