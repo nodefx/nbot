dcc4f9430e2f4fa868303c9fa2be6584fcb5ef15
ken <kenxu@mazing.com>
2017-04-27 18:30:51 +0800

    v0.1.2

--- deploy/push.js
+++ deploy/push.js
@@ -6,15 +6,15 @@ const gitEvent = process.argv[2]
 const cmd = function (...arg) {
   return new Promise((resolve, reject) => {
     exec(...arg, function (e, o, oe) {
-      if (e){
+      if (e) {
         console.error(e)
         return reject(e)
       }
-      else if(oe){
+      else if (oe) {
         console.error(oe)
         return reject(oe)
       }
-      else if(o){
+      else if (o) {
         console.log(o)
         return resolve(o)
       }
@@ -23,9 +23,7 @@ const cmd = function (...arg) {
 }
 
 async function queue() {
-
-    await cmd('git pull')
-    await cmd('yarn reload')
-
+  await cmd('git pull')
+  await cmd('yarn reload')
 }
 queue().then()
