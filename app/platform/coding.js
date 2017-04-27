dcb0744f45747f23baa4497bb1c9f035db0a1c0c
ken <kenxu@mazing.com>
2017-04-27 15:25:16 +0800

    v0.0.3

--- app/platform/coding.js
+++ app/platform/coding.js
@@ -2,9 +2,25 @@
  * Created by ken on 2017/4/26.
  */
 const db = require('../database')
+const {exec,spawn} = require('child_process')
 module.exports = async function(header={},get={},post={}){
   let gitEvent = Object.assign({},header,get,post)
-  if(gitEvent.event==='push'){
-    await db.model('push').insertAsync(gitEvent)
+  switch (gitEvent.event){
+    case 'push':
+      await gitpush(gitEvent)
+      break
   }
-} 
+}
+
+async function gitpush(gitEvent){
+  await db.model('push').insertAsync(gitEvent)
+  //exec(`node ${gitEvent.path}/deploy/push.js '${JSON.stringify(gitEvent)}'`)
+  let child = spawn(`node`, [`${gitEvent.path}/deploy/push.js`,`'${JSON.stringify(gitEvent)}'`]);
+  child.stdout.on('data', function (buffer) {
+    const d = buffer.toString()
+    console.log(d)
+  });
+  child.stdout.on('end', function () {
+
+  });
+}
---
+++ deploy/push.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ken on 2017/4/27.
+ */
+const {exec} = require('child_process')
+//console.log(JSON.parse(process.argv[2]||{}))
+//console.log('process.argv[2]',process.argv[2])
+console.log('push')
