363ff304191d03c682f97323c6e8c13741a5e99c
ken <kenxu@mazing.com>
2017-04-27 16:26:23 +0800

    v0.1.1

--- app/platform/index.js
+++ app/platform/index.js
@@ -2,7 +2,6 @@
  * Created by ken on 2017/4/27.
  */
 module.exports = async function(header,get,post){
-  console.log(header,get,post)
   switch (header['user-agent']){
     case 'Coding.net Hook':
       await require('./coding')(header,get,post)
