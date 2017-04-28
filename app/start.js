fea2080659b16e546279aa4c12991b3222b97b43
ken <kenxu@mazing.com>
2017-04-28 17:57:25 +0800

    v0.5

--- app/start.js
+++ app/start.js
@@ -7,4 +7,5 @@ const fs = require('fs')
 if (!fs.existsSync(conf.process.path.root)) {
   fs.mkdirSync(conf.process.path.root)
 }
+
 exec(`nohup node index.js > ${conf.process.path.access} 2>&1 & echo $! > ${conf.process.path.pid}`)
