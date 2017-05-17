df539262287bc37cbb6f05b5939bb3bf0c60f1d0
saa <saa@mazing.com>
2017-05-17 20:21:03 +0800

    首页

--- web/src/pages/home/index/index.js
+++ web/src/pages/home/index/index.js
@@ -4,18 +4,25 @@
 import React from 'react'
 import {Link} from 'react-router'
 import {inject, observer} from 'store/index'
+import {Icon, Card} from 'antd'
+import styles from './index.less'
 const storeName = {
   home: 'home/index'
 }
 @inject('appStore')
 @observer
 export default class extends React.Component {
-  componentWillMount(){
+  componentWillMount() {
     this.props.appStore.register(storeName)
     this.props.appStore.store[storeName.home].listen()
   }
+
   componentWillUnmount() {
     this.props.appStore.store[storeName.home].removeListen()
+  }
+
+  uptime(uptime) {
+    return parseInt(uptime / 86400) + '天' + parseInt(uptime % 86400 / 3600) + '小时' + parseInt(uptime % 86400 % 3600 / 60) + '分钟' + parseInt(uptime % 86400 % 3600 % 60) + '秒';
   }
 
   render() {
@@ -24,6 +31,10 @@ export default class extends React.Component {
       <div>
         <h1>nodejs devops!</h1>
         <h2>{JSON.stringify(data)}</h2>
+        <Card className={styles.cardItem} title='运行时间'>
+          <Icon type="clock-circle-o"/>
+          {this.uptime(data.uptime)}
+        </Card>
       </div>
     )
   }
